dfa79cba555a1a490a39a540635f06d456cdb40c
Manu Rueda <jm.glezderueda@gmail.com>
2022-03-02 15:37:01 -0500

    Updated Nuke dep version to 10.3

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/GetStream/stream-swift", .upToNextMajor(from: "2.2.0")),
-        .package(url: "https://github.com/kean/Nuke", .upToNextMajor(from: "8.1.0")),
+        .package(url: "https://github.com/kean/Nuke", .upToNextMajor(from: "10.3.0")),
         .package(url: "https://github.com/AliSoftware/Reusable.git", .upToNextMajor(from: "4.1.0")),
         .package(url: "https://github.com/SnapKit/SnapKit", .upToNextMajor(from: "5.0.0")),
     ],
